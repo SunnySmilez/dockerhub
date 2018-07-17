95289c1bc78cd87dcfe9207e7ef5c3065bc90673
zhouzhi <zhouzhi@360.cn>
2018-07-17 20:27:36 +0800

    crontab

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -8,4 +8,20 @@ else
     # 默认值
     RUN_ENV="dev"
     ln -sf /usr/local/php/lib/php.dev.ini /usr/local/php/lib/php.ini
+    if [ "php-fpm" = "$1" ]; then
+        service crond start
+        crontab /data/htdocs/"$APP_NAME"/bin/crontab
+    fi
+fi
+
+if [ "php-fpm" = "$1" ]; then
+    touch /tmp/php.log && chmod 666 /tmp/php.log
+    nohup tail -f /tmp/php.log &
+    exec "$@"
+elif [ "php" = "$1" ];then
+    exec "$@"
+elif [ "phpunit" = "$1" ];then
+    exec "$@"
+else
+    echo "error : run mode not in php-fpm,php,phpunit"
 fi
